2028d6406e30e86594fb5099fce427e0b6466fa6
SistEducativo30 <54105373+SistEducativo30@users.noreply.github.com>
2019-08-21 09:33:07 -0300

    Add files via upload

---
+++ src/app/menuprincipal/menuprincipal.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, share } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-menuprincipal',
+  templateUrl: './menuprincipal.component.html',
+  styleUrls: ['./menuprincipal.component.css']
+})
+export class MenuprincipalComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      share()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
